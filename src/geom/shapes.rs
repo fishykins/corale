860c545ba52dad008686791ccad89f65707d18fd
Fishykins <caspar.m.green@gmail.com>
2020-08-18 18:41:51 +0100

    Added Torus to shapes

--- src/geom/shapes.rs
+++ src/geom/shapes.rs
@@ -7,18 +7,18 @@ pub trait Cube<T> where T: OrdNum {
 }
 
 pub trait Sphere<T> where T: OrdNum {
-    fn radius(&self) -> T;
     fn center(&self) -> Vec3<T>;
+    fn radius(&self) -> T;
 }
 
 pub trait Cylinder<T> where T: OrdNum {
-    fn radius(&self) -> T;
     fn center(&self) -> Vec3<T>;
+    fn radius(&self) -> T;
     fn height(&self) -> T;
 }
 
-pub trait Triangle<T> where T: OrdNum {
-    fn a(&self) -> Vec3<T>;
-    fn b(&self) -> Vec3<T>;
-    fn c(&self) -> Vec3<T>;
+pub trait Torus<T> where T: OrdNum {
+    fn center(&self) -> Vec3<T>;
+    fn major(&self) -> T;
+    fn minor(&self) -> T;
 }
